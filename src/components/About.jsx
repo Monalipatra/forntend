7e5f3929ee08a7bba9f15d91642cd06de77b780a
Monalipatra <163519248+Monalipatra@users.noreply.github.com>
2024-04-12 16:21:01 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Style from "./css/Navbar.module.css";
+
+const About =() => {
+    return (
+    <>
+        <Navbar/>
+        <p className{Style.customText}>
+    </p>
+    hello from css
+    )
+
+}
